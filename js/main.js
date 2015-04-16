cc03c74045d0c6b3b57ab9439563077023dd8364
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2015-04-16 13:09:49 -0430

    ease text on display

--- js/main.js
+++ js/main.js
@@ -10,5 +10,6 @@ $(document).ready(function(){
 		$('.square').removeClass('open');
 		$('.square').css('transition', 'width 0s','-moz-transition', 'width 0s','-webkit-transition', 'width 0s');
 		$(this).parent().parent().parent().next().removeClass('next');
+		$('.square .more_info').css('transition', 'all 0s','-moz-transition', 'all 0s','-webkit-transition', 'all 0s')
 	});
 });
