57e85a8b023fb2c42164a8f71f8032ac3a391b1f
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2015-04-16 15:04:33 -0430

    hold functionality

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 		$(this).parent().parent().parent().addClass('open');
 		$('.square').css('transition', 'width 0.5s','-moz-transition', 'width 0.5s','-webkit-transition', 'width 0.5s');
 		$(this).parent().parent().parent().next().addClass('next');
+		$('.hold').show();
 	});
 	
 	$('.square-close').click(function() {
@@ -11,5 +12,15 @@ $(document).ready(function(){
 		$('.square').css('transition', 'width 0s','-moz-transition', 'width 0s','-webkit-transition', 'width 0s');
 		$(this).parent().parent().parent().next().removeClass('next');
 		$('.square .more_info').css('transition', 'all 0s','-moz-transition', 'all 0s','-webkit-transition', 'all 0s')
+		$('.hold').hide();
 	});
+
+	$('.hold').click(function() {
+		$('.hold').hide();
+		$('.square').removeClass('open');
+		$('.square').css('transition', 'width 0s','-moz-transition', 'width 0s','-webkit-transition', 'width 0s');
+		$('.square-close').parent().parent().parent().next().removeClass('next');
+		$('.square .more_info').css('transition', 'all 0s','-moz-transition', 'all 0s','-webkit-transition', 'all 0s')
+	});
+
 });
