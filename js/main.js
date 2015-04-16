f9f6eaf99b9f554fa0a18b726d32765c12c8c68e
AngelAlarcon <adalarcon@stcsoltions.com.ve>
2015-04-16 12:57:07 -0430

    Despliegue de slides avanzado (minor bug to be fixed)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
 
-	$('.square').click(function() {
-		$(this).addClass('open');
+	$('.read_more a').click(function() {
+		$(this).parent().parent().parent().addClass('open');
+		$('.square').css('transition', 'width 0.5s','-moz-transition', 'width 0.5s','-webkit-transition', 'width 0.5s');
+		$(this).parent().parent().parent().next().addClass('next');
 	});
 	
 	$('.square-close').click(function() {
 		$('.square').removeClass('open');
+		$('.square').css('transition', 'width 0s','-moz-transition', 'width 0s','-webkit-transition', 'width 0s');
+		$(this).parent().parent().parent().next().removeClass('next');
 	});
 });
